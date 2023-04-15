5471d1971a798a128ef73e765a7752495ea24d4c
eita-nakamura <eita.nakamura@gmail.com>
2023-04-16 03:08:04 +0900

    Add files via upload

--- ExpS2.js
+++ ExpS2.js
@@ -1,11 +1,10 @@
-var expID="S2G1";
+var expID="S2G2";
 var expTag="ExpS2";
-var expFolder="S2G1_x3327x";
-var headSentence="AI作曲家のメロディー聴き比べ　第2シーズン 第1世代";
+var expFolder="S2G2_x8515x";
+var headSentence="AI作曲家のメロディー聴き比べ　第2シーズン 第2世代";
 
-var candModel=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49];
-var candSong=[1489,2867,1825,573,2850,274,453,2882,989,1459,1831,2918,1273,2818,1946,587,1751,643,231,1839];
-
+var candModel=[0,4,6,10,11,16,17,18,19,21,22,23,27,29,31,32,34,37,38,41,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74];
+var candSong=[1454,256,2859,1829,1460,19,1997,870,225,2324,944,2095,2433,2762,466,1192,1339,1746,2133,967];
 
 document.getElementById("head_sentence").innerHTML="<font color=\"#ffffff\">"+headSentence+"</font>";
 
@@ -30,6 +29,7 @@ var curRatingA=0;
 var curDesriptionA=descriptions[0];
 var curRatingB=0;
 var curDesriptionB=descriptions[0];
+
 
 var status = "inquiry";
 //inquiry or selecting or selected
